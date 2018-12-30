1617b9770378ff8118bbd8d0decec5cdf3d34508
Manuel Hinterreiter <m.hinterreiter@countit.at>
2018-12-30 18:25:12 +0100

    initial project setup (closes #1)

---
+++ app/src/main/java/com/example/canteenchecker/canteenmanager/CanteenManagerApplication.java
@@ -0,0 +1,38 @@
+package com.example.canteenchecker.canteenmanager;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class CanteenManagerApplication extends Application {
+
+    private static final String FIREBASE_MESSAGING_TOPIC = "canteens";
+    private static CanteenManagerApplication instance;
+    public static CanteenManagerApplication getInstance() { return instance; }
+    private String authenticationToken;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+        // subscribe to FCM topic to receive push messages
+        FirebaseApp.initializeApp(this);
+        FirebaseMessaging.getInstance().subscribeToTopic(FIREBASE_MESSAGING_TOPIC);
+    }
+
+    public synchronized String getAuthenticationToken() {
+        return authenticationToken;
+    }
+
+    public synchronized void setAuthenticationToken(String authenticationToken) {
+        this.authenticationToken = authenticationToken;
+    }
+
+    public synchronized boolean isAuthenticated() {
+        return getAuthenticationToken() != null;
+    }
+
+
+}
